614c9fb7e023c66b0876a09b3884f215df61e9fe
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-07 18:03:06 +0300

    addRiddleController

--- controllers/riddles.controllers.js
+++ controllers/riddles.controllers.js
@@ -1,6 +1,18 @@
-import { getAllRiddles } from "../services/riddles.services.js";
+import { getAllRiddles, addRiddle } from "../services/riddles.services.js";
 
-export async function getRiddlesController(req, res){
+export async function getRiddlesController(req, res) {
     const riddles = await getAllRiddles();
     res.end(riddles);
+}
+
+export function addRiddleController(req, res) {
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    req.on("end", async () => {
+        body = JSON.parse(body);
+        res.end(await addRiddle(body));
+    })
 }
