a738484b912daf93be8a831c52ed5b8aec3c3c5f
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-07 12:34:00 +0300

    dal write riddles

--- DAL/riddles.dal.js
+++ DAL/riddles.dal.js
@@ -1,7 +1,11 @@
-import { readFile } from "fs/promises"
+import { readFile, writeFile } from "fs/promises"
 
 const riddlesDBPath = "C:/Users/Chaim0533197133/OneDrive/Documents/programming/projects/riddles-server/DB/riddles.txt";
 
 export async function readRiddles() {
     return await readFile(riddlesDBPath, "utf-8");
+}
+
+export async function writeRiddles(riddlesArray) {
+    writeFile(riddlesDBPath, riddlesArray);
 }
