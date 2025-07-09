cd12515251704a7a03335fd6279fbffb808f7f96
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-09 15:47:55 +0300

    handle catch router

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ import router from "./routers/riddles.router.js"
 
 const server = createServer(async (req, res) => {
     console.log(`method: ${req.method}, url: ${req.url}`)
+    try{
     await router[req.method][req.url](req, res)
+
+    } catch (err){
+        res.end(err.message)
+    }
 })
 
 server.listen(1456, () => {
