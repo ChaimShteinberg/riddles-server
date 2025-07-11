ac6e6245b7e8a2069453c384912236dec1c4bee4
ChaimShteinberg <198433514+ChaimShteinberg@users.noreply.github.com>
2025-07-11 10:42:43 +0300

    Update path riddles.dal.js

--- DAL/riddles.dal.js
+++ DAL/riddles.dal.js
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from "fs/promises"
 
-const riddlesDBPath = "C:/Users/Chaim0533197133/OneDrive/Documents/programming/projects/riddles-server/DB/riddles.txt";
+const riddlesDBPath = "../DB/riddles.txt";
 
 export async function readRiddles() {
     return await readFile(riddlesDBPath, "utf-8");
