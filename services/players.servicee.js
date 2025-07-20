f30807385ccac8604e4888d355659dfdc4f6a9eb
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-20 14:28:54 +0300

    update player supabase

--- services/players.servicee.js
+++ services/players.servicee.js
@@ -7,34 +7,16 @@ export async function getAllPlarers() {
 }
 
 export async function addPlayer(newPlayer) {
-    const {error} = await playerDB.insert(newPlayer)
+    const { error } = await playerDB.insert(newPlayer)
     if (error) return error;
     return "ok"
 }
 
-export async function updatePlayer(update) {
-    let file = await getAllPlarers();
-    try {
-        if (file === "The database is empty") {
-            throw new Error(file);
-        } else {
-            file = JSON.parse(file)
-        }
-        for (const player of file) {
-            if (player.id === update.id) {
-                const updateKeys = Object.keys(update);
-                for (const key of updateKeys) {
-                    player[key] = update[key];
-                }
-                await writeplayers(JSON.stringify(file, null, 2));
-                return;
-            }
-        }
-        throw new Error("The player is not fount");
-
-    } catch (err) {
-        return err
-    }
+export async function updatePlayer(username, best_time) {
+    const { error } = await playerDB.update({"best_time": best_time})
+        .eq('username', username)
+    if (error) return error;
+    return "ok"
 }
 
 export async function deletePlayer(id) {
