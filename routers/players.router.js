7353177f1db8b1cf602c886e76bacf6353774f45
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-23 12:23:05 +0300

    signin route

--- routers/players.router.js
+++ routers/players.router.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addPlayerController, getPlayersController, updatePlayerController, leaderboardControler, singupController } from "../controllers/players.controllers.js";
+import { addPlayerController, getPlayersController, updatePlayerController, leaderboardControler, singupController, signinController } from "../controllers/players.controllers.js";
 
 const playersRouter = express.Router();
 
-playersRouter.post('/signup', singupController)
+playersRouter.post('/signup', singupController);
+
+playersRouter.post('/signin', signinController)
 
 playersRouter.get('/getAll', getPlayersController);
 
